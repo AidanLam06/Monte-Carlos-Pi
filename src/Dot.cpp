26280ce7d16650b63f83e375e2c8953482ec18d6
Aidan Lam <aidanlam02@gmail.com>
2026-03-15 23:31:56 -0400

    Remove build/ and .vscode metadata

--- src/Dot.cpp
+++
@@ -1,7 +0,0 @@
-#include "Dot.hpp"
-
-Dot::Dot(double x_val, double y_val) : x(x_val), y(y_val) {
-    double dx = x_val - XC;
-    double dy = y_val - YC;
-    is_inside = (dx * dx + dy * dy) < (RADIUS * RADIUS);
-}
